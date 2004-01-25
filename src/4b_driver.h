50e13c411ac4fad151a97953d4f869a056e52e03
Odd Skancke <oskancke@getmail.no>
2004-01-25 21:39:27 +0000

    *** empty log message ***

--- src/4b_driver.h
+++
@@ -1,8 +0,0 @@
-#ifndef _STDDRV_4B_H
-#define _STDDRV_4B_H
-
-#include "ovdi_defs.h"
-
-extern pixel_blit dpf_4b[];
-
-#endif	/* _STDDRV_4B_H */
--- src/ikbd_mouse.h
+++
@@ -1,7 +0,0 @@
-#ifndef _IKBD_MOUSE_H
-#define _IKBD_MOUSE_H
-
-#include "mouse.h"
-
-
-#endif	/* _IKBD_MOUSE_H */
